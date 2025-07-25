d0c7d9b8f5597570497fe7ff3dd46a6a9cd330f5
Buksim <koredsam700@gmail.com>
2025-07-25 20:11:33 +0100

    Add files via upload

--- Mood Detector.py
+++ Mood Detector.py
@@ -1,14 +1,29 @@
 from textblob import TextBlob
 Name = input("\nName: ")
+Name = Name.capitalize()
 while True:
     Sentence = input("Sentence (Or 'Exit'): ")
     if Sentence.lower() == 'Exit':
         print("Bye!", Name)
         break
     Polarity = TextBlob(Sentence).sentiment.polarity
-    if Polarity > 0:
-        print("\U0001F642", Polarity)
-    elif Polarity < 0:
-        print("\U0001F641", Polarity)
+    if Polarity > 0.8:
+        print(f"😍 {Name} Your Sentiment Polarity Is {Polarity}")
+    elif Polarity > 0.6:
+        print(f"🥰 {Name} Your Sentiment Polarity Is {Polarity}")
+    elif Polarity > 0.4:
+        print(f"😘 {Name} Your Sentiment Polarity Is {Polarity}")
+    elif Polarity > 0.2:
+        print(f"😊 {Name} Your Sentiment Polarity Is {Polarity}")
+    elif Polarity == 0:
+        print(f"😐 {Name} Your Sentiment Polarity Is {Polarity}")
+    elif Polarity < -0.2:
+        print(f"😔 {Name} Your Sentiment Polarity Is {Polarity}")
+    elif Polarity < -0.4:
+        print (f"😟 {Name} Your Sentiment Polarity Is {Polarity}")
+    elif Polarity < -0.6:
+        print (f"😢 {Name} Your Sentiment Polarity Is {Polarity}")
+    elif Polarity < -0.8:
+        print (f"😭 {Name} Your Sentiment Polarity Is {Polarity}")
     else:
-        print("\U0001F610", Polarity)
+        print (f"\U0001F610")
